ea88ff4c5380d4970315c5de766df327aa3a9dcd
Christina-Chau <christinachau5@gmail.com>
2021-04-27 01:32:03 -0400

    intro

--- app/src/main/java/com/example/itslit/MainActivity.java
+++ app/src/main/java/com/example/itslit/MainActivity.java
@@ -8,19 +8,60 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView countText;
+    private EditText messageText;
+    private Button send;
+    private ArrayList <String> str = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_screen);
         Toast.makeText(MainActivity.this, "Hello world", Toast.LENGTH_LONG).show();
+        messageText = findViewById(R.id.messageText);
+        countText = findViewById(R.id.countText);
+        send = findViewById(R.id.sendMessage);
+
+        //adjusts the display for the number of characters
+        messageText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                countText.setText((s.toString().length()) + "/75");
+            }
+        });
+
+        //sends text to arraylist on button click
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str.add(messageText.getText().toString());
+                messageText.setText("");
+            }
+        });
     }
 }
 //    @Override
@@ -35,7 +76,31 @@ public class MainActivity extends AppCompatActivity {
 //        // Handle action bar item clicks here. The action bar will
 //        // automatically handle clicks on the Home/Up button, so long
 //        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
+//        int id = item.getItemI
+//    }
+//}
+////    @Override
+////    public boolean onCreateOptionsMenu(Menu menu) {
+////        // Inflate the menu; this adds items to the action bar if it is present.
+////        getMenuInflater().inflate(R.menu.menu_main, menu);
+////        return true;
+////    }
+////
+////    @Override
+////    public boolean onOptionsItemSelected(MenuItem item) {
+////        // Handle action bar item clicks here. The action bar will
+////        // automatically handle clicks on the Home/Up button, so long
+////        // as you specify a parent activity in AndroidManifest.xml.
+////        int id = item.getItemId();
+////
+////        //noinspection SimplifiableIfStatement
+////        if (id == R.id.action_settings) {
+////            return true;
+////        }
+////
+////        return super.onOptionsItemSelected(item);
+////    }
+////d();
 //
 //        //noinspection SimplifiableIfStatement
 //        if (id == R.id.action_settings) {
